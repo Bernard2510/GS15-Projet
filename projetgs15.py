b9d4b51ec2db690d5ff1487b0d068a1a38e630bc
RomaneLucas <romane.95570@gmail.com>
2022-11-27 20:10:50 +0100

    Update projetgs15.py
    RSA_1

--- projetgs15.py
+++ projetgs15.py
@@ -1,3 +1,21 @@
-TEST GIT
-SALUT
-Test roro
+def sign_rsa(priv_key,M):
+
+    p=random_prime() #ou fixe ?
+    q=random_prime()
+    n=p*q
+
+    while (n%priv_key==0): #ou verifer premier entre eux
+        p=random_prime()
+        q=random_prime()
+        n=p*q
+
+    S=M**priv_key % n
+    return S
+
+
+def verify_rsa(pub_key,S,M,n):
+    M1 = (S**pub_key) % n
+    if (M1==M):
+        return True
+    else:
+        return False
