8373ae1d17a34807193e44ef846f51ac6b380f25
RomaneLucas <romane.95570@gmail.com>
2022-12-05 13:58:01 +0100

    Update feistel.py

--- feistel.py
+++ feistel.py
@@ -5,21 +5,27 @@ def file_to_binary(filename_path):
         return bin_data
 
 
-def circularPermutation(K):
-    # initialisation de la liste obtenue via une permutation circulaire de L
+def circularPermutation_left(K): #permutation 2 bits
     circlarString = ""
     for i in range(len(K)-2):
         circlarString=circlarString+""+K[i+2]
     circlarString=circlarString+""+K[0]
     circlarString=circlarString+""+K[1]
+    return circlarString
 
+def circularPermutation_rigth(K): #permutation 2 bits
+    circlarString = ""
+    circlarString=circlarString+""+K[len(K)-1]
+    circlarString=circlarString+""+K[len(K)-2]
+    for i in range(len(K)-2):
+        circlarString=circlarString+""+K[i]
     return circlarString
 
 def func_fesitel(K,D):
     res = int(K,base=2)+int(D,base=2) % 2**16
     return "{0:b}".format(int(res))
 
-def feistel(data):
+def feistel_encrypt(data):
     K0="0000000011111111"
 
     if len(data)%2!=0: #pas utile normalement
@@ -31,7 +37,7 @@ def feistel(data):
         G0 = G0+""+data[i]
         D0 = D0+""+data[i+int(len(data)/2)]
 
-    T=2
+    T=3
     for i in range(T):
         G1=D0
         D1=int(G0,base=2)^int(func_fesitel(K0,D0),base=2)
@@ -42,14 +48,37 @@ def feistel(data):
         
         G0=G1
         D0=D1
-        K0=circularPermutation(K0)
+        K0=circularPermutation_left(K0)
+    
+    return G1+""+D1
+    
 
-    print(G1)
-    print(D1)
+def feistel_decrypt(data):
+    K1 = "0000111111110000"
 
+    G1=""
+    D1=""
+    for i in range(int(len(data)/2)):
+        G1 = G1+""+data[i]
+        D1 = D1+""+data[i+int(len(data)/2)]
+
+    T=3
+    for i in range(T):
+        G0=int(D1,base=2)^int(func_fesitel(K1,G1),base=2)
+        G0="{0:b}".format(int(G0))
+        D0=G1
+        
+        while len(G0)!=len(data)/2:
+            G0="0"+G0
+        
+        G1=G0
+        D1=D0
+        K1=circularPermutation_rigth(K1)
+    
+    return G0+""+D0
 
 
 #feistel(file_to_binary("C:\\Users\\roman\\OneDrive\\Documents\\GitHub\\GS15-Projet\\server\\test\\test.txt"))
-#print(circularPermutation("0000000011111111"))
-#print(func_fesitel("01001","11"))
-feistel("01000111010100110011000100110101")
+
+print(feistel_encrypt("01000111010100110011000100110101"))
+print(feistel_decrypt("01001000010101100010110100100001"))
