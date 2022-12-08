5ab0aa8d0164c42f5a3a795dbe87fd161f09317e
Bernard2510 <bernardle28@gmail.com>
2022-12-08 18:20:43 +0100

    v

--- testgs15.py
+++ testgs15.py
@@ -1,4 +1,5 @@
 import random
+import hashlib
 
 class Utilisateur:
 
@@ -48,3 +49,25 @@ def hmac_sha256(chainkey,data):
 
 
 #hmac_sha256(1,1)
+# a = 123456789
+# print(a)
+# m = hashlib.sha256(str(a).encode('ASCII')).hexdigest()
+# print(m)
+
+
+def signRSA(priv_key,M):
+
+    p=random_prime() #ou fixe ?
+    q=random_prime()
+    n=p*q
+
+    while (n%priv_key==0): #ou verifer premier entre eux
+        p=random_prime()
+        q=random_prime()
+        n=p*q
+
+    S=M**priv_key % n
+    return S
+
+def initRSAkey():
+    return 
